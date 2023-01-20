6b682eb245284383880de93db8250cc81d2aab79
Darya <iontseva.dasha>
2023-01-20 14:19:53 +0300

    Example010_MethodArray

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,6 +1,6 @@
-﻿string username = Console.ReadLine();
+﻿string? username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username.ToLower() == "маша") 
 {
 
     Console.WriteLine("Ура, это же МАША!");
